94374695d63f1745b07c0e35bc8535b5175aec48
Andra Satria Pratama <andrasatriapratama@gmail.com>
2020-11-12 16:58:39 +0700

    destructuring

---
+++ ReactJs Dasar/03.ES6/code/02.destructuring.js
@@ -0,0 +1,11 @@
+const data = {
+    nama: "andra",
+    umur: "26",
+    pekerjaan: "programmer"
+}
+
+/* Destructuring = Menstruktur ulang */
+const { nama, umur, pekerjaan } = data;
+console.log(nama);
+console.log(umur);
+console.log(pekerjaan);
