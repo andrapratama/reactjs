551ffd8077c7a9ec94ca7f1d6c958d03012a851e
Andra Satria Pratama <andrasatriapratama@gmail.com>
2020-11-20 19:26:36 +0700

    template literal

---
+++ ReactJs Dasar/03.ES6/code/04.templateliteral.js
@@ -0,0 +1,11 @@
+// template literal
+
+const nama = "Andra";
+const umur = 23;
+const pekerjaan = "Software Engineer"
+
+// tanpa template literal
+console.log('Nama saya adalah ' + nama + ' dan umur saya ' + umur + ' serta pekerjaan saya ' + pekerjaan);
+
+// dengan template literal
+console.log(`Nama saya adalah ${nama} dan umur saya ${umur} serta pekerjaan saya ${pekerjaan}`);
